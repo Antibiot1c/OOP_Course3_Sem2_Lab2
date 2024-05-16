61a5015c68feec7698e8ab51059bf13e153f088f
Pavlo Velychko <112926321+Antibiot1c@users.noreply.github.com>
2024-05-16 15:05:07 +0300

    Create BookService.java

---
+++ library/service/BookService.java
@@ -0,0 +1,41 @@
+package com.example.library.service;
+
+import com.example.library.model.Book;
+import com.example.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> searchBooks(String title) {
+        return bookRepository.findByTitleContaining(title);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return optionalBook.orElse(null);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        if (bookRepository.existsById(id)) {
+            book.setId(id);
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
